f4b4d573fc2a5dc65da96f3df17e7737371872c2
Gülçin Bodur <gbodur@k1m08s01.42istanbul.com.tr>
2026-01-04 13:41:16 +0300

    feat(bonus): add player direction vector to minimap - Implement 'draw_minimap_line' using DDA algorithm for efficient line drawing. - Add 'draw_player_direction' to visualize player orientation on the minimap. - Update 'render_minimap' to layer the direction line over the player position.

--- bonus/graphics_bonus/minimap_bonus.c
+++ bonus/graphics_bonus/minimap_bonus.c
@@ -3,14 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   minimap_bonus.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdivan <mdivan@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*   By: gbodur <gbodur@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 16:04:29 by gbodur            #+#    #+#             */
-/*   Updated: 2026/01/03 04:01:03 by mdivan           ###   ########.fr       */
+/*   Updated: 2026/01/04 13:37:18 by gbodur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
+
+static void	draw_minimap_line(t_engine *engine, int x1, int y1, int x2, int y2)
+{
+	double	delta_x;
+	double	delta_y;
+	double	steps;
+	double	x;
+	double	y;
+
+	delta_x = x2 - x1;
+	delta_y = y2 - y1;
+	if (fabs(delta_x) > fabs(delta_y))
+		steps = fabs(delta_x);
+	else
+		steps = fabs(delta_y);
+	delta_x /= steps;
+	delta_y /= steps;
+	x = x1;
+	y = y1;
+	while (steps >= 0)
+	{
+		if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
+			renderer_put_pixel(engine->renderer, (int)x, (int)y, 0xFF0000);
+		x += delta_x;
+		y += delta_y;
+		steps--;
+	}
+}
+
+static void	draw_player_direction(t_engine *engine)
+{
+	int	center_x;
+	int	center_y;
+	int	end_x;
+	int	end_y;
+	int	line_len;
+
+	line_len = MINIMAP_SCALE * 2;
+	center_x = MINIMAP_PAD + (int)(engine->character->pos.x * MINIMAP_SCALE);
+	center_y = MINIMAP_PAD + (int)(engine->character->pos.y * MINIMAP_SCALE);
+	end_x = center_x + (int)(engine->character->dir.x * line_len);
+	end_y = center_y + (int)(engine->character->dir.y * line_len);
+	draw_minimap_line(engine, center_x, center_y, end_x, end_y);
+}
 
 static void	draw_minimap_square(t_engine *engine, int x, int y, int color)
 {
@@ -87,4 +131,5 @@ void	render_minimap(t_engine *engine)
 		y++;
 	}
 	draw_player_on_minimap(engine);
+	draw_player_direction(engine);
 }
